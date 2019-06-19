31ad9df58b0b7942bbdec9f9c1a552a3afa4474e
jia <jiahy@uci.edu>
2019-06-18 19:55:29 -0700

    check

--- main.js
+++ main.js
@@ -32,6 +32,22 @@ class blockChain{
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length;++i){
+            const current = this.chain[i];
+            const prev = this.chain[i-1];
+            
+            if(current.hash !== current.calculateHash()){
+                return false;
+            }
+
+            if(current.preHash !== prev.hash){
+                return false;
+            }
+        }
+        return true;
+    }
 }
 
 let yaoCoin = new blockChain();
@@ -39,3 +55,7 @@ yaoCoin.addBlock(new block(1,"6/18/19",{amount:4}));
 yaoCoin.addBlock(new block(2,"6/18/19",{amount:10}));
 
 console.log(JSON.stringify(yaoCoin,null,4));
+console.log(yaoCoin.isChainValid());
+yaoCoin.chain[1].data = {amount: 100};
+yaoCoin.chain[1].hash = yaoCoin.chain[1].calculateHash();
+console.log(yaoCoin.isChainValid());
