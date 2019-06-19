a89bc2a8efb6dd118076be698969a227c780fff8
jia <jiahy@uci.edu>
2019-06-18 20:36:14 -0700

    proof of work and rewards

--- main.js
+++ main.js
@@ -1,36 +1,80 @@
 const SHA256 = require('crypto-js/sha256')
 
+class transaction{
+    constructor(from, to, amount){
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+    }
+}
+
+
 class block{
-    constructor(index,timestamp,data,preHash=''){
-        this.index = index;
+    constructor(timestamp,transaction,preHash=''){
         this.timestamp = timestamp;
-        this.data = data;
+        this.transaction = transaction;
         this.preHash = preHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash(){
-        return SHA256(this.index + this.preHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.preHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficult){
+        while(this.hash.substring(0, difficult) !== Array(difficult+1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log(this.hash);
     }
 }
 
 class blockChain{
     constructor(){
         this.chain = [this.createGenesisBlock()];
+        this.difficult = 2;
+        this.pendingTransactions = [];
+        this.miningReward = 100;
     }
 
     createGenesisBlock(){
-        return new block(0,'01/01/2019',"Genesis blck", "0");
+        return new block('01/01/2019',"Genesis block", "0");
     }
 
     getLatestBlock(){
         return this.chain[this.chain.length-1]
     }
 
-    addBlock(newBlock){
-        newBlock.preHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
-        this.chain.push(newBlock);
+    mingPending(miningRewardAddress){
+        let newblock = new block(Date.now(), this.pendingTransactions);
+        newblock.mineBlock(this.difficult);
+        console.log('got the block');
+        this.chain.push(newblock);
+
+        this.pendingTransactions = [new transaction(null, miningRewardAddress, this.miningReward)];
+    }
+
+    createTransaction(transaction){
+        this.pendingTransactions.push(transaction);
+    }
+
+    getBalanceOfAddress(address){
+        let balance = 0;
+        for (const block of this.chain){
+            console.log(block);
+            for(const tran of block.transaction){
+                if (tran.from === address){
+                    balance -= tran.amount;
+                }
+
+                if (tran.to === address){
+                    balance += tran.amount;
+                }
+            }
+        }
+        return balance;
     }
 
     isChainValid(){
@@ -50,12 +94,13 @@ class blockChain{
     }
 }
 
-let yaoCoin = new blockChain();
-yaoCoin.addBlock(new block(1,"6/18/19",{amount:4}));
-yaoCoin.addBlock(new block(2,"6/18/19",{amount:10}));
+let jyCoin = new blockChain();
 
-console.log(JSON.stringify(yaoCoin,null,4));
-console.log(yaoCoin.isChainValid());
-yaoCoin.chain[1].data = {amount: 100};
-yaoCoin.chain[1].hash = yaoCoin.chain[1].calculateHash();
-console.log(yaoCoin.isChainValid());
+jyCoin.createTransaction(new transaction('address1','address2',100));
+jyCoin.createTransaction(new transaction('address2','address1',50));
+jyCoin.mingPending('123');
+console.log(jyCoin.getBalanceOfAddress('123'));
+jyCoin.mingPending('123');
+console.log(jyCoin.getBalanceOfAddress('123'));
+jyCoin.mingPending('123');
+console.log(jyCoin.getBalanceOfAddress('123'));
